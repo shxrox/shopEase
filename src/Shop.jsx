2336a28ada2b921f2d446ba826f895c04aa3c0f0
shxrox <sharondeva8@gmail.com>
2025-04-04 18:21:25 +0530

    category

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -5,17 +5,16 @@ import NewArrivals from './components/Sections/NewArrivals';
 import content from './data/content.json';
 import Category from './components/Sections/Categories/Category';
 
-
 const Shop = () => {
   return (
     <>
       <Navigation />
       <HeroSection />
       <NewArrivals />
-      {content.categories?.length > 0 && (
-        <Category title={content.categories[0].title} data={content.categories[0].data} />
-        
-      )}
+      {content.categories?.length > 0 &&
+        content.categories.map((category, index) => (
+          <Category key={index} title={category.title} data={category.data} />
+        ))}
     </>
   );
 };
